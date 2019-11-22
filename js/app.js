cd432b5e033952eb42f4fd49b3df6a982ea5ed74
Nekiedra Singleton <40775404+Nshandrell98@users.noreply.github.com>
2019-11-22 10:14:41 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,64 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newTask() {
+  var li = document.createElement("li");
+  var task = document.getElementById("task").value;
+  var newT = document.createTextNode(task);
+  li.appendChild(newT);
+  if (task === '') {
+    alert("Give me a task!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("task").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+// removing tasks when 
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+document.addEventListener('keydown', event =>{
+  if (event.keyCode === 32){
+      newTask();
+  }
+  else if (event.keyCode === 13){
+    newTask();
+}
+  
+})
